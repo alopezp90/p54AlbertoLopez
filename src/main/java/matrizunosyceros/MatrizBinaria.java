c7e12175e52d83bab22c0221f1621dc34b8a0f93
alopezp90 <alopezp90@gmail.com>
2021-03-08 14:34:52 +0100

    Modifica condicion logica

--- src/main/java/matrizunosyceros/MatrizBinaria.java
+++ src/main/java/matrizunosyceros/MatrizBinaria.java
@@ -52,13 +52,9 @@ public class MatrizBinaria {
 
     private boolean compruebaAdyacentes(int i, int j) {
         for (int m = i - 1; m <= i + 1; m++) {
-             for (int n = j - 1; n <= j + 1; n++) {
-                if (m >= 0 && m < matriz.length && n >= 0 && n < matriz.length) { //es decir, esta dentro de la matriz
-                    if (matriz[m][n] == 0) {  //si encuentra un 0
-                        if (m != i || n != j) { //no es en (i,j)
-                            return false;
-                        }
-                    }
+            for (int n = j - 1; n <= j + 1; n++) {
+                if (m >= 0 && m < matriz.length && n >= 0 && n < matriz.length && matriz[m][n] == 0 && (m != i || n != j)) { //es decir, esta dentro de la matriz
+                    return false;
                 }
             }
         }
@@ -68,21 +64,20 @@ public class MatrizBinaria {
     //Prueba
     public static void main(String[] args) {
         int[] resultado;
-        final int[] NULO = new int[]{-1,-1};
-        
-        
+        final int[] NULO = new int[]{-1, -1};
+
         for (int i = 0; i < 10; i++) {
             MatrizBinaria prueba = new MatrizBinaria(5);
             System.out.println(prueba);
-            
+
             resultado = prueba.buscaPosicionCeroRodeadoDeUnos();
-            
-            if(Arrays.equals(resultado, NULO)) {
+
+            if (Arrays.equals(resultado, NULO)) {
                 System.out.println("Ninguna coordenada cumple los requisitos");
             } else {
-                System.out.println("Coordenada que cumple: " + Arrays.toString(resultado));
+                System.out.println("Primera coordenada que cumple: " + Arrays.toString(resultado));
             }
-            
+
             System.out.println("--------------------------------------");
         }
     }
